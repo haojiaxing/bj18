046f54f7c5c87c5e5304a5c03bd407358ebee781
hjx <1037540162@qq.com>
2022-02-25 20:02:44 +0800

    login

--- views.py
+++ views.py
@@ -1,5 +1,7 @@
 from django.http import HttpResponse
-
+from djiang.shortcuts import redirect
 
 def index(request):
     return HttpResponse('index')
+def index(request):
+    return redirect('index')
